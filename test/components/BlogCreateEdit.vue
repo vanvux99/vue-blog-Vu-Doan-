32d8495c5182524cbb3019c397862acf6f98f1b2
vũ <vanvux99@gmail.com>
2021-03-01 02:13:22 +0700

    update ngay 1 thang 3

--- test/components/BlogCreateEdit.vue
+++ test/components/BlogCreateEdit.vue
@@ -1,133 +1,134 @@
+
 <template>
   <div class="container-fluid">
     <h3>{{ title }}</h3>
-<form>
-    <!-- title -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Tiêu đề</label>
-      <div class="col-10">
-        <input class="w-75 form-control" id="title" v-model="form.title" type="text" />
+    <form>
+      <!-- title -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Tiêu đề</label>
+        <div class="col-10">
+          <input class="w-75 form-control" id="title" v-model="form.title" type="text" />
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- description -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Mô tả ngắn</label>
-      <div class="col-10">
-        <input class="w-75 form-control" id="description" v-model="form.des" type="text" />
+      <!-- description -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Mô tả ngắn</label>
+        <div class="col-10">
+          <input class="w-75 form-control" id="description" v-model="form.des" type="text" />
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- detail -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Chi tiết</label>
-      <div class="col-10">
-        <textarea
-          v-model="form.detail"
-          rows="10"
-          class="form-control"
-          id="detail"
-        ></textarea>
+      <!-- detail -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Chi tiết</label>
+        <div class="col-10">
+          <textarea
+            v-model="form.detail"
+            rows="10"
+            class="form-control"
+            id="detail"
+          ></textarea>
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- thumbs -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Hình ảnh</label>
-      <div class="col-10">
-        <button>Choose files</button>
-        <label> Không có tệp nào được chọn </label>
+      <!-- image -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Hình ảnh</label>
+        <div class="col-10">
+          <button>Choose files</button>
+          <label> Không có tệp nào được chọn </label>
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- category -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Loại</label>
-      <div class="col-10">
-        <select id="caterogy" v-model="form.category">
-          <option
-            v-for="category in categories"
-            :value="category.value"
-            :key="category.value"
-          >
-            {{ category.text }}
-          </option>
-        </select>
+      <!-- category -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Loại</label>
+        <div class="col-10">
+          <select id="caterogy" v-model="form.category">
+            <option
+              v-for="category in categories"
+              :value="category.value"
+              :key="category.value"
+            >
+              {{ category.text }}
+            </option>
+          </select>
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- position -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Vị trí</label>
-      <div class="col-10"
-            v-for="itemCheckbox in positions" 
-            :value="itemCheckbox.value"
-            :key ="itemCheckbox.value"
-      >
-        <input type="checkbox" :id="'cb_'+itemCheckbox.value" :value="itemCheckbox.value" v-model="form.position" />
-        <label :for="'cb_'+itemCheckbox.value">{{itemCheckbox.text}}</label>
-        <br />
+      <!-- position -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Vị trí</label>
+        <div class="col-10"
+              v-for="itemCheckbox in positions" 
+              :value="itemCheckbox.value"
+              :key ="itemCheckbox.value"
+        >
+          <input type="checkbox" :id="'cb_'+itemCheckbox.value" :value="itemCheckbox.value" v-model="form.position" />
+          <label :for="'cb_'+itemCheckbox.value">{{itemCheckbox.text}}</label>
+          <br />
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- public -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Public</label>
-      <div class="col-10"
-            v-for="item in publics" 
-            :value="item.value"
-            :key ="item.value"
-      >
-        <input type="radio" :id="'pl_'+item.value " :value="item.value" v-model="form.public" />
-        <label :for="item.value">{{item.text}}</label>
-        <br />
+      <!-- public -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Public</label>
+        <div class="col-10"
+              v-for="item in publics" 
+              :value="item.value"
+              :key ="item.value"
+        >
+          <input type="radio" :id="'pl_'+item.value " :value="item.value" v-model="form.public" />
+          <label :for="item.value">{{item.text}}</label>
+          <br />
+        </div>
+        <span class="messError"></span>
       </div>
-       <span class="messError"></span>
-    </div>
 
-    <!-- date -->
-    <div class="form-group">
-      <label class="col-2 ml-2">Date Public</label>
-      <div class="col-10">
-        <input type="date" v-model="form.data_pubblic" />
+      <!-- date -->
+      <div class="form-group">
+        <label class="col-2 ml-2">Date Public</label>
+        <div class="col-10">
+          <input type="date" v-model="form.data_pubblic" />
+        </div>
+        <span class="messError"></span>
       </div>
-      <span class="messError"></span>
-    </div>
-    
-    <!-- button -->
-    <div class="form-group d-flex justify-content-center">
-      <!--  create button -->
-      <button
-        v-if="title === 'New Blogs'"
-        type="button"
-        class="btn btn-success mr-2"
-        @click="postBlog()" 
-      >
-        Submit
-      </button>
+      
+      <!-- button -->
+      <div class="form-group d-flex justify-content-center">
+        <!--  create button -->
+        <button
+          v-if="title === 'New Blogs'"
+          type="button"
+          class="btn btn-success mr-2"
+          @click="postBlog()" 
+        >
+          Submit
+        </button>
 
-      <!-- edit button -->
-      <button
-        v-if="title === 'Edit Blogs'"
-        type="button"
-        class="btn btn-success mr-2"
-        @click="updateBlog(form, form.id)"
-      >
-        Submit
-      </button>
+        <!-- edit button -->
+        <button
+          v-if="title === 'Edit Blogs'"
+          type="button"
+          class="btn btn-success mr-2"
+          @click="updateBlog(form, form.id)"
+        >
+          Submit
+        </button>
 
-      <button type="button" @click="clear()" class="btn btn-primary">
-        Clear
-      </button>
+        <button type="button" @click="clear()" class="btn btn-primary">
+          Clear
+        </button>
 
-      <button type="button" @click="a()">TEST</button>
-    </div>
+        <button type="button" @click="a()">TEST</button>
+      </div>
     </form>
   </div>
 </template>
@@ -196,39 +197,35 @@ export default {
   },
 
   methods: {
-    // test: function() {
-      
-
-    //   // let form = document.querySelectorAll(".form-group")
-    //   // let element = document.querySelector("input")
-
-    //   // for (let index = 0; index < form.length; index++) {
-    //   //     if (form[index].querySelector("input") !=null) {
-    //   //       if(form[index].querySelector("input").type == "text") {
-    //   //         return element;
-    //   //     }
-    //   //   }
-    //   // }
-    // },
-
-    // a: function() {
-    //   let form = document.querySelector("#title").type
-    //   console.log(form);
-    // },
-
-    // element truyền vào, id của elemnt đó ????
-    getParent: function() {
-
+    
+    /** 
+     * @author Vu Doan
+     * @param  element: called element
+     * @param  selector: selector to class ".form-group"
+     * @return parentElement have class ".form-group"
+     * 
+     * @since 1-3-2021
+     */
+    getParent: function(element, selector) {
+      while (element.parentElement) {
+          if (element.parentElement.matches(selector)) {
+            return element.parentElement
+          }
+        element = element.parentElement
+      }
     },
 
+    /** check valid element into form
+     * @author Vu Doan
+     * @param  id id for element input
+     * @return parentElement have class ".form-group"
+     * 
+     * @since 1-3-2021
+     */
     checkValid: function(id) {
+      let form = document.querySelector("form")
       let checkId = document.querySelector(id)
 
-      while (checkId.parentElement.querySelector(".messError")) {
-        let erorElement = Element.parentElement.querySelector(".messError")
-        break;
-      }
-        console.log(checkId.parentElement);
       if(checkId) 
       { 
         let checkType = checkId.type
@@ -236,8 +233,10 @@ export default {
         if(getType)
         { 
           let value = document.querySelector(id).value
-
-          // let err = checkId.parentElement.querySelector(".messError")
+          let formGroup = form.querySelector(".form-group")
+          let err = this.getParent(checkId, formGroup)
+          console.log(err);
+          err.querySelector(".messError");
           if(value == null){
             return err.innerHTML = ""
           }
@@ -291,10 +290,6 @@ export default {
       //axios.put(source_link + id, data);
       alert("Sửa thành công")
       this.$router.push('/blogs/list')
-    },
-
-    abc: function (){
-      alert("sdfsdf")
     },
   },
 };
